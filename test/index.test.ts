8c88dbe0b7d28e8148b0fc44fbb2947e30878fcf
Erick Zhao <erick@hotmail.ca>
2022-05-12 16:13:55 -0700

    test: add additional test cases

--- test/index.test.ts
+++ test/index.test.ts
@@ -13,19 +13,34 @@ describe("Local Versioner", () => {
     console.error = jest.fn();
   });
 
-  it("fetches the version from the default branch", async () => {
+  it("generates the correct version from the default branch", async () => {
     const latestVersion = await v.getVersionForHead();
     expect(latestVersion).toBe("4.2.4");
   });
 
-  it("returns 65535 patch number for feature branch off of trunk", async () => {
-    const version = await v.getVersionForCommit("499537a");
-    const parsed = semver.parse(version);
-    expect(parsed?.patch).toBe(0xffff);
-  });
+  it.todo(
+    "generates the correct version for the default branch before any release"
+  );
 
-  it("returns 65535 patch number for feature branch off of release branch", async () => {
-    const version = await v.getVersionForCommit("57deab3");
+  it.each([
+    ["the tip of a release branch (release-4.1.x)", "ece1c3f", "4.1.8"],
+    ["a commit on a release branch (release-4.1.x)", "c95b710", "4.1.7"],
+    ["from a different major version (release-3.2.x)", "21c5383", "3.2.3"],
+    ["the first minor release branch (release-1.0.x)", "b2bcb99", "1.0.2"],
+  ])(
+    "generates the correct version from %s",
+    async (_, sha, expectedVersion) => {
+      const version = await v.getVersionForCommit(sha);
+      const parsed = semver.parse(version);
+      expect(parsed?.version).toBe(expectedVersion);
+    }
+  );
+
+  it.each([
+    ["trunk", "499537a"],
+    ["release branch", "57deab3"],
+  ])("returns 65535 patch number for branch off of %s", async (_, sha) => {
+    const version = await v.getVersionForCommit(sha);
     const parsed = semver.parse(version);
     expect(parsed?.patch).toBe(0xffff);
   });
