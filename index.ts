93c9e30387cdbe3d62a07a1d6c2d45c5655cf5d4
Erick Zhao <erick@hotmail.ca>
2022-05-27 14:03:33 -0700

    chore: fix class export capitalization

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ import LocalVersioner from "./src/version-local";
 import GitHubVersioner from "./src/version-github";
 
 export const Versioner = {
-  local: LocalVersioner,
-  github: GitHubVersioner,
+  Local: LocalVersioner,
+  GitHub: GitHubVersioner,
 };
